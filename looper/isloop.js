d7f41b2de409bbafd56a001d4e138fab3c8cd665
Carina Belino <cbelino18@gmail.com>
2022-10-27 15:52:30 -0400

    added isLoop & findLoop

--- looper/isloop.js
+++ looper/isloop.js
@@ -1,10 +1,33 @@
-'use strict';
+"use strict";
 
 //Complete this algo
 const isLoop = (linkedlist) => {
-
+  const nodesContained = [];
+  let currentNode = linkedlist.head;
+  while (currentNode.next) {
+    if (nodesContained.includes(currentNode)) {
+      return true;
+    }
+    nodesContained.push(currentNode);
+    currentNode = currentNode.next;
+    // console.log(nodesContained);
+  }
+  return false;
 };
 
+const findLoop = (linkedlist) => {
+  const nodesContained = [];
+  let currentNode = linkedlist.head;
+  while (currentNode.next) {
+    if (nodesContained.includes(currentNode)) {
+      return currentNode;
+    }
+    nodesContained.push(currentNode);
+    currentNode = currentNode.next;
+    // console.log(nodesContained);
+  }
+  return false;
+};
 
 /*
 EXTRA CREDIT:
@@ -14,4 +37,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = { isLoop, findLoop };
