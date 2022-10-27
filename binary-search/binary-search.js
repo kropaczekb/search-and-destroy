1684b70852740916180cef03c3d7721753d69938
Brian Kropaczek <kropaczekb@gmail.com>
2022-10-27 17:32:32 -0400

    Extra credit passing tests

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,22 +2,44 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-		const midpoint = Math.floor(array.length/2)
-		if (array[midpoint] === target) {
-			return true;
-		} else {
-			if (array.length === 1 || array.length === 0) {
-				return false;
-			}
-			if (target < array[midpoint]) {
-				const newArray = array.slice(0, midpoint)
-				return binarySearch(newArray, target)
-			}
-			if (array[midpoint] < target) {
-				const newArray = array.slice((midpoint+1))
-				return binarySearch(newArray, target)
+		const embeddedSearch = (start, end) => {
+			const midpoint = (Math.floor((end - start)/2) + start)
+			if (array[midpoint] === target) {
+				return true
+			} else {
+				if (end - start < 1) {
+					return false
+				}
+
+				if (target < array[midpoint]) {
+					let newEnd = Math.floor(end-midpoint-1)
+					return embeddedSearch(start, newEnd)
+				}
+
+				if (array[midpoint] < target) {
+					let newStart = Math.floor(midpoint+1)
+					return embeddedSearch(newStart, end)
+				}
 			}
 		}
+		const endingPoint = array.length -1
+		return embeddedSearch(0, endingPoint)
+		// const midpoint = Math.floor(array.length/2)
+		// if (array[midpoint] === target) {
+		// 	return true;
+		// } else {
+		// 	if (array.length === 1 || array.length === 0) {
+		// 		return false;
+		// 	}
+		// 	if (target < array[midpoint]) {
+		// 		// const newArray = array.slice(0, midpoint)
+		// 		return binarySearch(array, target)
+		// 	}
+		// 	if (array[midpoint] < target) {
+		// 		// const newArray = array.slice((midpoint+1))
+		// 		return binarySearch(array, target)
+		// 	}
+		// }
 };
 
 /*
